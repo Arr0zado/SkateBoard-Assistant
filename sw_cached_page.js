9f5700b3749beaaefd1f0e7cba9a0a0b732eb9dd
Arr0zado <55147716+Arr0zado@users.noreply.github.com>
2020-10-02 00:08:34 +0100

    test pwa builder 4

--- sw_cached_page.js
+++ sw_cached_page.js
@@ -50,7 +50,7 @@ self.addEventListener('fetch', e => {
 
 */
 
-const CACHE = "pwabuilder-offline";
+const CACHE = "pwabuilder - v1";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -66,3 +66,42 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 );
+
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
