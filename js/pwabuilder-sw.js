efdc7b84cb46a5fe2bf9975af681e3588bb3bf03
Arr0zado <55147716+Arr0zado@users.noreply.github.com>
2020-10-01 14:12:16 +0100

    try to fix service worker

--- js/pwabuilder-sw.js
+++ js/pwabuilder-sw.js
@@ -1,8 +1,4 @@
-// This is the service worker with the Cache-first network
-
-// This is the "Offline copy of assets" service worker
-
-const CACHE = "pwabuilder-offline";
+const CACHE = "pwabuilder-precache";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -14,7 +10,7 @@ self.addEventListener("message", (event) => {
 
 workbox.routing.registerRoute(
   new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
+  new workbox.strategies.CacheFirst({
     cacheName: CACHE
   })
 );
